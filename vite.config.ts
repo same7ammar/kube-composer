b9aa74c55dcee1a8f3e69dab6237e10233191779
Sameh ammar <mr.same7.ammar@gmail.com>
2025-06-11 06:04:15 +0300

    Configure for custom domain kube-composer.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: '/', // Root path for custom domain
   build: {
     outDir: 'dist',
     sourcemap: false,
