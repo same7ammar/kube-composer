0a37c43f9453c70ec3f7853b5e77aa2a49c4534e
Sameh ammar <mr.same7.ammar@gmail.com>
2025-06-11 06:00:05 +0300

    Fix Vite build paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kube-composer/',
+  base: './', // Use relative paths for GitHub Pages
   build: {
     outDir: 'dist',
     sourcemap: false,
